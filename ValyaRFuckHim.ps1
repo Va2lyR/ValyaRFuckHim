c7fa28dc7b9fab08119fe843caf6320cd0bd93e3
Va2lyR <Va2lyR@users.noreply.github.com>
2026-09-21 14:24:46 +0300

    Add Valyar subcategory with JarAnalyzer

--- ValyaRFuckHim.ps1
+++ ValyaRFuckHim.ps1
@@ -144,7 +144,8 @@ $ToolData = @(
     @{ Name="StringChecker";         Desc="Lafferrs Strings Checker";                            Category="Other-Others"; Type="GitHub"; URL="https://github.com/Lafferrr/SSTools/releases/latest" },
     @{ Name="Java Library Analyzer"; Desc="Lafferr Java Library Analyzer";                       Category="Other-Others"; Type="GitHub"; URL="https://github.com/Lafferrr/SSTools/releases/latest" },
     @{ Name="PJ Cheat Scanner Lite"; Desc="String Checker by gorbgallin";                        Category="Other-Others"; Type="GitHub"; URL="https://github.com/gorbgallin/Pj-sCheatScannerLite/releases/latest" },
-    @{ Name="JarAnalyzer";           Desc="JAR file analyzer and decompiler";                     Category="Other-Others"; Type="Web";    URL="https://github.com/Va2lyR/ValyaRFuckHim/releases/download/ss/JarAnalyzer.exe" },
+
+    @{ Name="JarAnalyzer";           Desc="JAR file analyzer and decompiler";                     Category="Other-Valyar"; Type="Web";    URL="https://github.com/Va2lyR/ValyaRFuckHim/releases/download/ss/JarAnalyzer.exe" },
 
     @{ Name="NET 8.0";               Desc="Microsoft .NET 8 SDK runtime";                        Category="Other-Dependencies"; Type="Web"; URL="https://dotnet.microsoft.com/en-us/download/dotnet/thank-you/sdk-8.0.423-windows-x64-installer" },
 
@@ -878,7 +879,7 @@ function Show-SourceCode {
 
 $Categories = @("Mod Analyzer","Clients Detector","Other")
 
-$OtherSubCategories = @("Orbdiff","Spokwn","Tonynoh","Praiselily","RedLotus","DetectAC","TeslaPro","Echo","TRSSCommunity","Magnet","Forensics","SystemTools","Analysis","Misc","NirSoft","Zimmerman","Scripts","Others","Dependencies")
+$OtherSubCategories = @("Valyar","Orbdiff","Spokwn","Tonynoh","Praiselily","RedLotus","DetectAC","TeslaPro","Echo","TRSSCommunity","Magnet","Forensics","SystemTools","Analysis","Misc","NirSoft","Zimmerman","Scripts","Others","Dependencies")
 
 foreach ($cat in $Categories) {
     $tab = New-Object System.Windows.Controls.TabItem
